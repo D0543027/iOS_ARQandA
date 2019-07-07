7926cbbf73a24c4d636901e3b09d6a589a7ca819
higana34266199 <j34266199@gmail.com>
2019-07-07 15:13:59 +0800

    launch新增儲存空間 profile寫入資訊(回答總題數,總答對數,總答錯數,準確率,單次遊玩最高分) QA更改與新增存取

--- ARQandA/ProfileViewController.swift
+++ ARQandA/ProfileViewController.swift
@@ -9,61 +9,64 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    var status = ""
-    var statusList = [0,0,0,0,0,0,0]
+    var statusName = ""
+    var statusDate = ""
+    var statusList = [0,0,0,0,0,0]
+    let right = UserDefaults.standard.integer(forKey: "right")
+    let wrong = UserDefaults.standard.integer(forKey: "wrong")
     override func viewDidLoad() {
         super.viewDidLoad()
-        status = ""
+        statusName = ""
+        statusDate = ""
         statusList[0] = 0
-        statusList[1] = 0
-        statusList[2] = 0
-        statusList[3] = 0
-        statusList[4] = 0
-        statusList[5] = Int(statusList[3]/statusList[2])
-        statusList[6] = 0
+        statusList[1] = right + wrong
+        statusList[2] = right
+        statusList[3] = wrong
+        statusList[4] = Int(statusList[3]/statusList[2])
+        statusList[5] = UserDefaults.standard.integer(forKey: "highScore")
         // Do any additional setup after loading the view.
     }
     
     func tableView1(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ProfileTableViewCell {
         let cell = ProfileTableViewCell(style: .default, reuseIdentifier: "name")
-        cell.nameLb.text = status
+        cell.nameLb.text = statusName
         return cell
     }
     func tableView2(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ProfileTableViewCell {
         let cell = ProfileTableViewCell(style: .default, reuseIdentifier: "beginDate")
-        cell.beginDateLb.text = String(statusList[0])
+        cell.beginDateLb.text = statusDate
         return cell
     }
     func tableView3(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ProfileTableViewCell {
         let cell = ProfileTableViewCell(style: .default, reuseIdentifier: "dayCount")
-        cell.dayCountLb.text = String(statusList[1])
+        cell.dayCountLb.text = String(statusList[0])
         return cell
     }
     func tableView4(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ProfileTableViewCell {
         let cell = ProfileTableViewCell(style: .default, reuseIdentifier: "total")
-        cell.totalLb.text = String(statusList[2])
+        cell.totalLb.text = String(statusList[1])
         return cell
     }
     func tableView5(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ProfileTableViewCell {
         let cell = ProfileTableViewCell(style: .default, reuseIdentifier: "right")
-        cell.rightLb.text = String(statusList[3])
+        cell.rightLb.text = String(statusList[2])
         return cell
     }
     func tableView6(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ProfileTableViewCell {
         let cell = ProfileTableViewCell(style: .default, reuseIdentifier: "wrong")
-        cell.wrongLb.text = String(statusList[4])
+        cell.wrongLb.text = String(statusList[3])
         return cell
     }
     
     func tableView7(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ProfileTableViewCell {
         let cell = ProfileTableViewCell(style: .default, reuseIdentifier: "accuracy")
-        cell.accuracyLb.text = String(statusList[5])
+        cell.accuracyLb.text = String(statusList[4])
         return cell
     }
     
     func tableView8(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ProfileTableViewCell {
         let cell = ProfileTableViewCell(style: .default, reuseIdentifier: "onceHScore")
-        cell.onceHScoreLb.text = String(statusList[6])
+        cell.onceHScoreLb.text = String(statusList[5])
         return cell
     }
     /*
