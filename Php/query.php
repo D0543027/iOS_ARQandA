6dfef59233e030682e4880b1b165a595a3dc524b
D0543027 <a0935999309@gmail.com>
2019-09-21 23:44:31 +0800

    PHP檔更新

--- Php/query.php
+++ Php/query.php
@@ -2,25 +2,22 @@
   $servername = "localhost";
   $username = "root";
   $password = "";
-  $database = "test";
+  $database = "ARQA";
   // Create connection
   $conn = mysqli_connect($servername, $username, $password,$database);
 
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-    $label = $_POST["label"];   //辨識物件名稱
+    $tablename = $_POST["tablename"];   //辨識物件名稱
     $num = (int) $_POST['num']; //難易度決定題數
 
     $sql = "SELECT * FROM 
-        (SELECT * FROM plant_questions ORDER BY RAND() ) AS result 
+        (SELECT * FROM $tablename ORDER BY RAND() ) AS result 
         LIMIT $num;";
-
 
     // Check if there are results
       if ($result = mysqli_query($conn, $sql))
       {
-      // If so, then create a results array and a temporary one
-      // to hold the data
       $resultArray = array();
       $tempArray = array();
 
@@ -30,12 +27,8 @@
         // Add each row into our results array
         $tempArray = $row;
         array_push($resultArray, $tempArray);
-  
       }
-
-      // Finally, encode the array to JSON and output the results
       echo json_encode($resultArray);
       }
   }
-  
 ?>
