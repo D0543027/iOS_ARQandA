3332a562b09038fa8e75f6ab9021e5ab7cedbfc9
D0543027 <a0935999309@gmail.com>
2019-10-08 13:26:37 +0800

    PHP更新

--- Php/query.php
+++ Php/query.php
@@ -7,13 +7,11 @@
   $conn = mysqli_connect($servername, $username, $password,$database);
 
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-    $tablename = $_POST["tablename"];   //辨識物件名稱
-    $num = (int) $_POST['num']; //難易度決定題數
+    $label = $_POST['label'];
 
     $sql = "SELECT * FROM 
-        (SELECT * FROM $tablename ORDER BY RAND() ) AS result 
-        LIMIT $num;";
+            (SELECT  * FROM qa_label WHERE label = '$label' ORDER BY RAND()) AS result
+            LIMIT 5;";
 
     // Check if there are results
       if ($result = mysqli_query($conn, $sql))
